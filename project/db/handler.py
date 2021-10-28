04e4feea36669ef434ca7381438b74419766d889
Дмитрий Покладов <PokladovDV@gmail.com>
2021-10-28 19:59:52 +0300

    Срок годности рефреш-токена

--- project/db/handler.py
+++ project/db/handler.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, datetime
+
 import pymysql
 
 from project.classes.User import User
@@ -25,9 +27,9 @@ class DBHandler:
         with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
                              db=self.db) as conn:
             curs = conn.cursor()
-            curs.execute("SELECT * FROM user WHERE user_id='{}'".format(user_id))
+            curs.execute("SELECT user_id, login, pass_hash, group_id FROM shop.user WHERE user_id='{}'".format(user_id))
             row = curs.fetchone()
-            curs.execute("SELECT name FROM shop.group WHERE group_id='{}'".format(row[5]))
+            curs.execute("SELECT name FROM shop.group WHERE group_id='{}'".format(row[3]))
             roles = curs.fetchone()
             user = User(user_id=row[0], login=row[1], pass_hash=row[2], roles=roles)
 
@@ -46,7 +48,11 @@ class DBHandler:
         with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
                              db=self.db) as conn:
             curs = conn.cursor()
-            curs.execute("SELECT * FROM user WHERE user_id='{}'".format(user_id))
-            row = curs.fetchone()
-            token = row[3]
+            curs.execute("SELECT refresh_token, token_date FROM shop.user WHERE user_id='{}'".format(user_id))
+            token, date = curs.fetchone()
+
+            shift = timedelta(hours=480)
+            if (date + shift) < datetime.today():
+                return None
+
             return token
