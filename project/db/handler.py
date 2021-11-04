02abd334a8b9959013d520d884d9b8dd5a981899
Дмитрий Покладов <PokladovDV@gmail.com>
2021-11-05 02:43:41 +0300

    Запрос и изменение данных пользователя

--- project/db/handler.py
+++ project/db/handler.py
@@ -58,15 +58,20 @@ class DBHandler:
 
             return token
 
-    def set_new_user(self, user_json):
+    def set_new_user(self, user_json, group):
         with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
                              db=self.db) as conn:
             curs = conn.cursor()
 
             pass_hash = sha256(bytes(user_json["password"], encoding="utf-8")).hexdigest()
+            if user_json['birth_date'] == "":
+                birth_date = str(datetime.today())
+            else:
+                birth_date = user_json['birth_date']
+
             sql_string = "INSERT INTO shop.user SET login='{}', pass_hash='{}', firstname='{}', lastname='{}'," \
                          "phone_number='{}', address_city='{}', address_street='{}', address_build='{}'," \
-                         "address_apartment='{}', mail='{}', birth_date=NULL, group_id = 4"
+                         "address_apartment='{}', mail='{}', birth_date='{}', group_id='{}'"
             sql_string = sql_string.format(user_json['login'],
                                            pass_hash,
                                            user_json[
@@ -84,21 +89,65 @@ class DBHandler:
                                            user_json['address'][
                                                'apartment'],
                                            user_json['mail'],
-                                           user_json[
-                                               'birth_date'])
+                                           birth_date,
+                                           group)
             curs.execute(sql_string)
             conn.commit()
 
+    def get_user_data(self, user_id):
+        with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
+                             db=self.db) as conn:
+            curs = conn.cursor()
+            curs.execute(
+                "SELECT login, firstname, lastname, phone_number, address_city, address_street, address_build, " +
+                "address_apartment, mail, birth_date FROM shop.user WHERE user_id='{}'".format(
+                    user_id))
+            login, firstname, lastname, phone_number, address_city, address_street, address_build, address_apartment, \
+            mail, birth_date = curs.fetchone()
+            return {
+                'mail'        : mail,
+                'login'       : login,
+                'firstname'   : firstname,
+                'lastname'    : lastname,
+                'phone_number': phone_number,
+                'address'     : {
+                    'city'     : address_city,
+                    'street'   : address_street,
+                    'build'    : address_build,
+                    'apartment': address_apartment, 'mail': mail,
+                },
+                'birth_date'  : str(birth_date)
+            }
 
-def get_user_data(self, user_id):
-    with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
-                         db=self.db) as conn:
-        curs = conn.cursor()
-        curs.execute(
-            "SELECT  FROM shop.user WHERE user_id='{}'".format(user_id))
-        row = curs.fetchone()
-        curs.execute("SELECT name FROM shop.group WHERE group_id='{}'".format(row[3]))
-        roles = curs.fetchone()
-        user = UserAuth(user_id=row[0], login=row[1], pass_hash=row[2], roles=roles)
+    def edit_user_data(self, user_json, user_id):
+        print(user_id)
+        with pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd,
+                             db=self.db) as conn:
+            pass_hash = sha256(bytes(user_json["password"], encoding="utf-8")).hexdigest()
 
-    return user
+            curs = conn.cursor()
+            curs.execute(
+                "UPDATE shop.user SET login='{}', pass_hash='{}', firstname='{}', lastname='{}', "
+                "phone_number='{}', address_city='{}', address_street='{}', address_build='{}', "
+                "address_apartment='{}', mail='{}', "
+                "birth_date='{}' WHERE user_id='{}'".format(user_json['login'],
+                                                            pass_hash,
+                                                            user_json[
+                                                                'firstname'],
+                                                            user_json[
+                                                                'lastname'],
+                                                            user_json[
+                                                                'phone_number'],
+                                                            user_json['address'][
+                                                                'city'],
+                                                            user_json['address'][
+                                                                'street'],
+                                                            user_json['address'][
+                                                                'build'],
+                                                            user_json['address'][
+                                                                'apartment'],
+                                                            user_json['mail'],
+                                                            user_json['birth_date'],
+                                                            user_id)
+            )
+            conn.commit()
--- project/routes.py
+++ project/routes.py
@@ -1,4 +1,5 @@
-from sanic.response import text
+from sanic.response import text, json
+from sanic_jwt import protected, scoped, inject_user
 
 from project.db.handler import DBHandler
 
@@ -20,15 +21,85 @@ def setup_routes(app):
     # async def user(request, *args, **kwargs):
     #     return json(status=200, body={"name": "user"})
 
+    @app.route("/reg/admin", methods=["POST"])
+    @protected()
+    @scoped(["admin"], False)
+    async def reg_admin(req):
+        message = req.json
+
+        if message['login'] == "" or message['password'] == "":
+            return text(status=400, body="Missing password or login")
+
+        db = DBHandler()
+        db.set_new_user(message, 1)
+        del db
+
+        return text(status=200, body="OK")
+
+    @app.route("/reg/owner", methods=["POST"])
+    @protected()
+    @scoped(["admin"], False)
+    async def reg_owner(req):
+        message = req.json
+
+        if message['login'] == "" or message['password'] == "":
+            return text(status=400, body="Missing password or login")
+
+        db = DBHandler()
+        db.set_new_user(message, 2)
+        del db
+
+        return text(status=200, body="OK")
+
+    @app.route("/reg/seller", methods=["POST"])
+    @protected()
+    @scoped(["admin", "owner"], False)
+    async def reg_seller(req):
+        message = req.json
+
+        if message['login'] == "" or message['password'] == "":
+            return text(status=400, body="Missing password or login")
+
+        db = DBHandler()
+        db.set_new_user(message, 3)
+        del db
+
+        return text(status=200, body="OK")
+
     @app.route("/reg/user", methods=["POST"])
     async def reg_user(req):
         message = req.json
 
-        if message['mail'] == "" or message['password'] == "":
+        if message['login'] == "" or message['password'] == "":
             return text(status=400, body="Missing password or login")
 
         db = DBHandler()
-        db.set_new_user(message)
+        db.set_new_user(message, 4)
+        del db
+
+        return text(status=200, body="OK")
+
+    @app.route("/get_user_data", methods=["GET"])
+    @inject_user()
+    @protected()
+    async def get_user_data(req, user):
+        db = DBHandler()
+        data = db.get_user_data(user.user_id)
+        del db
+
+        return json(status=200, body=data)
+
+    @app.route("/change_user_data", methods=["POST"])
+    @inject_user()
+    @protected()
+    async def change_user_data(req, user):
+        message = req.json
+
+        if message['login'] == "" or message['password'] == "":
+            return text(status=400, body="Missing password or login")
+
+        db = DBHandler()
+        db.edit_user_data(message, user.user_id)
         del db
 
         return text(status=200, body="OK")
